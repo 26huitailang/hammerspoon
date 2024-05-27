5d4f68f8f51060077f487a81c50a8f231c6e4ac6
26huitailang <26huitailang@gmail.com>
2024-05-27 09:14:06 +0800

    update AppLaucher

--- init.lua
+++ init.lua
@@ -2,21 +2,25 @@ hs.loadSpoon("ReloadConfiguration")
 hs.loadSpoon("SpoonInstall")
 spoon.ReloadConfiguration:start()
 
+
 spoon.SpoonInstall:andUse("AppLauncher", {
     hotkeys = {
-        t = "iTerm",
-        c = "Calendar",
         b = "Firefox",
-        v = "Visual Studio Code",
+        c = "Calendar",
         d = "DingTalk" ,
+        f = "Finder" ,
+        t = "iTerm",
+        v = "Visual Studio Code",
+        w = "wechat",
+        n = "Notion",
+        o = "Obsidian",
     }
 })
 
 -- 空间
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-    local ret = hs.spaces.activeSpaces()
-    hs.alert.show(ret)
-    local ret = hs.spaces.allSpaces()
-    hs.alert.show(ret)
-end)
-
+-- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+--     local ret = hs.spaces.activeSpaces()
+--     hs.alert.show(ret)
+--     local ret = hs.spaces.allSpaces()
+--     hs.alert.show(ret)
+-- end)
